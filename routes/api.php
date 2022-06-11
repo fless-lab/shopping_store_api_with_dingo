1c4da158fd389815727f9ac60fe3d11887cb5b40
flesslab <achilleatarmla@gmail.com>
2022-06-11 03:44:11 +0000

    User register done

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+$api = app("Dingo\Api\Routing\Router");
+
+$api->version('v1', function ($api) {
+
+    $api->get("/",function(){
+        return "Welcome to the Shopping Store API !!!";
+    });
+
+    $api->post("/users/register",[UserController::class,"store"]);
+    $api->post("/users/login",[AuthController::class,"login"]);
+
 });
